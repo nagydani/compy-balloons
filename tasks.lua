0fbc3c16fda6ba48c331acd1bafe0c111c11c455
Daniel A. Nagy <nagy.da@gmail.com>
2026-05-14 10:22:24 +0200

    Simpler problems

--- tasks.lua
+++ tasks.lua
@@ -1,18 +1,24 @@
 require("graphics")
 
 TASKS_CONFIG = {
-  { q = "Print missing letter in 'giraf..e':", a = "f" },
-  { q = "Print missing letter in 'car..ot':", a = "r" },
-  { q = "Print missing letter in 'eleph..nt':", a = "a", style = "orange" },
-  { q = "Print missing letter in 'ban..na':", a = "a", style = "orange" },
-  { q = "Print missing letter in 'do..key':", a = "n", style = "yellow" },
-  { q = "Print missing letter in 'spi..er':", a = "d", style = "yellow" },
-  { q = "Print missing letter in 'butt..rfly':", a = "e", style = "green" },
-  { q = "Print missing letter in 'chi..ken':", a = "c", style = "green" },
-  { q = "What is 3 + 4?:", a = "7", size = 1.25, speed = 0.75, style = "red" },
-  { q = "What is 9 - 5?:", a = "4", size = 1.25, speed = 0.75, style = "red" },
-  { q = "How many legs does a dog have?:", a = "4" },
-  { q = "How many days are in a week?:", a = "7" },
+  { q = "C", r = "c" },
+  { q = "F", r = "f" },
+  { q = "G", r = "g" },
+  { q = "E", r = "e" },
+  { q = "U", r = "u" },
+  { q = "O", r = "o" },
+  { q = "GIRAF.E", a = "f" },
+  { q = "CA.ROT", a = "r" },
+  { q = "ELEPH.NT", a = "a", style = "orange" },
+  { q = "BAN.NA", a = "a", style = "orange" },
+  { q = "DO.KEY", a = "n", style = "yellow" },
+  { q = "SPI.ER", a = "d", style = "yellow" },
+  { q = "BUTT.RFLY", a = "e", style = "green" },
+  { q = "C", a = "c", style = "green" },
+  { q = "3+4", a = "7", size = 1.25, speed = 0.75, style = "red" },
+  { q = "9-5", a = "4", size = 1.25, speed = 0.75, style = "red" },
+  { q = "4", a = "4" },
+  { q = "7", a = "7" },
 }
 
 TASKS = {}
